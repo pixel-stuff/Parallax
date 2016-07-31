427efe8c8e9325a08e6f8ecc0edc9ed5bfb086ff
Jeremy Morel <jeremy.morel.jm@gmail.com>
2016-07-31 20:45:55 +0200

    mise en place de la camera mouvante

---
+++ Assets/Resources/MoveTestScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTestScript : MonoBehaviour {
+
+	public float moveX;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.D)) {
+			Move (moveX);
+		}
+
+		if (Input.GetKey (KeyCode.Q)) {
+			Move (-moveX);
+		}
+	}
+
+	void Move(float xMove){
+		this.gameObject.transform.position = new Vector3 (gameObject.transform.position.x + xMove,gameObject.transform.position.y,gameObject.transform.position.z);
+	}
+}
