869ba3f92eec435fb47374e06a567b94b0c652e6
jmorel <jmorel@betomorrow.com>
2016-08-12 14:06:48 +0200

    change testCamera script for Up/down move

--- Assets/Resources/MoveTestScript.cs
+++ Assets/Resources/MoveTestScript.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class MoveTestScript : MonoBehaviour {
 
 	public float moveX;
+	public float moveY;
 	// Use this for initialization
 	void Start () {
 	
@@ -11,16 +12,31 @@ public class MoveTestScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		float finalMoveX = 0;
+		float finalMoveY = 0;
+
 		if (Input.GetKey (KeyCode.D)) {
-			Move (moveX);
+			finalMoveX += moveX;
 		}
 
 		if (Input.GetKey (KeyCode.Q)) {
-			Move (-moveX);
+			finalMoveX += -moveX;
+		}
+
+		if (Input.GetKey (KeyCode.Z)) {
+			finalMoveY += moveY;
+		}
+
+		if (Input.GetKey (KeyCode.S)) {
+			finalMoveY += -moveY;
+		}
+
+		if (finalMoveX != 0 || finalMoveY != 0) {
+			Move (finalMoveX, finalMoveY);
 		}
 	}
 
-	void Move(float xMove){
-		this.gameObject.transform.position = new Vector3 (gameObject.transform.position.x + xMove,gameObject.transform.position.y,gameObject.transform.position.z);
+	void Move(float xMove,float yMove){
+		this.gameObject.transform.position = new Vector3 (gameObject.transform.position.x + xMove,gameObject.transform.position.y+yMove,gameObject.transform.position.z);
 	}
 }
